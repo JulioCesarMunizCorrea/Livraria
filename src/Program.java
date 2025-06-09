c53a7ba80b58e6ab927013d9ea27d5c2eaaf7105
Juliao <juliaomc@gmail.com>
2025-06-09 15:51:58 -0300

    teste w11

---
+++ src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Oĺa eu sou Goku");
+		System.out.println("Testando eclipse");
+		System.out.println("teste w11");
+
+	}
+
+}
